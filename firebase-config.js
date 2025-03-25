efe3f17c72830a2b20435867529449f5b3e58d52
Huguitito <huguitito@gmail.com>
2025-03-25 09:19:52 -0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 // Reemplaza con TUS datos de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAHZnk75eHtRLLfHslO1QdYLY3gZUrutfs",
-  authDomain: "mqrtql.firebaseapp.com",
-  databaseURL: "https://mqrtql-default-rtdb.firebaseio.com",
-  projectId: "mqrtql",
-  storageBucket: "mqrtql.firebasestorage.app",
-  messagingSenderId: "711181799625",
-  appId: "1:711181799625:web:e5238821f52fc83a433d18"
-};
+    apiKey: "AIzaSyAHZnk75eHtRLLfHslO1QdYLY3gZUrutfs",
+    authDomain: "mqrtql.firebaseapp.com",
+    databaseURL: "https://mqrtql-default-rtdb.firebaseio.com",
+    projectId: "mqrtql",
+    storageBucket: "mqrtql.firebasestorage.app",
+    messagingSenderId: "711181799625",
+    appId: "1:711181799625:web:323213597c121794433d18"
+  };
 
 // Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
